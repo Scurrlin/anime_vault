cdde7f40b2373f565765e6f799b5938f399bd298
Sean Currlin <sean.currlin@gmail.com>
2024-08-30 19:19:49 -0400

    update LoadMore.tsx

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,27 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { fetchAnime } from "@/app/action";
+import { AnimeProp } from "./AnimeCard";
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+
+  useEffect(() => {
+    if(inView) {
+      fetchAnime(2).then((res) => {
+        setData([...data, ...res])
+      });
+    }
+  }, [inView, data]);
+
   return (
     <>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
