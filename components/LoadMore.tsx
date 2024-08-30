aefddf5476e5988eb5120f43a6c60efd0b432fe5
Sean Currlin <sean.currlin@gmail.com>
2024-08-30 19:24:48 -0400

    move AnimeProp section to LoadMore.tsx

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useInView } from "react-intersection-observer";
 import { fetchAnime } from "@/app/action";
-import { AnimeProp } from "./AnimeCard";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+let page = 2;
 
 function LoadMore() {
   const { ref, inView } = useInView();
@@ -20,6 +22,11 @@ function LoadMore() {
 
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
           <Image
