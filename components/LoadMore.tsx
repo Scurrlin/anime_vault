4aa906e543ce6dc68164ac04378d1266de370a18
Sean Currlin <sean.currlin@gmail.com>
2024-08-30 19:54:26 -0400

    update LoadMore.tsx

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,45 +1,59 @@
 "use client"
 
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useInView } from "react-intersection-observer";
-import { fetchAnime } from "@/app/action";
-import AnimeCard, { AnimeProp } from "./AnimeCard";
+import { useEffect, useState } from "react";
+
+import { fetchAnime } from "../app/action";
 
 let page = 2;
 
+export type AnimeCard = JSX.Element;
+
 function LoadMore() {
   const { ref, inView } = useInView();
-  const [data, setData] = useState<AnimeProp[]>([]);
+
+  const [data, setData] = useState<AnimeCard[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if(inView) {
-      fetchAnime(2).then((res) => {
-        setData([...data, ...res])
-      });
+    if (inView) {
+      setIsLoading(true);
+      const delay = 500;
+
+      const timeoutId = setTimeout(() => {
+        fetchAnime(page).then((res) => {
+          setData([...data, ...res]);
+          page++;
+        });
+
+        setIsLoading(false);
+      }, delay);
+
+      return () => clearTimeout(timeoutId);
     }
-  }, [inView, data]);
+  }, [inView, data, isLoading]);
 
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
+          {inView && isLoading && (
+            <Image
+              src="./spinner.svg"
+              alt="spinner"
+              width={56}
+              height={56}
+              className="object-contain"
+            />
+          )}
         </div>
       </section>
     </>
   );
 }
 
-export default LoadMore;
+export default LoadMore
